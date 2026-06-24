0853600720826a56ba72d4cc6b12e34bdee809c1
creatijourny <misbahulhaque.gmail.com>
2026-06-24 12:40:14 +0600

    jwt create and access

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 
@@ -16,13 +17,38 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+)
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization
+  if(!authHeader) {
+    return res.status(401).json({message:
+      "Unauthorized"});
+  }
+  const token = authHeader.split(' ')[1]
+  if(!token){
+    return res.status(401).json({message:
+      "Unauthorized"});
+  }
+
+  try {
+    const {payload} = await jwtVerify(token, JWKS)
+  console.log(payload)
+  next()
+  } catch (error) {
+    return res.status(403).json({message: "Forbidden1"});
+  }  
+}
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("studynest");
     const roomCollection = db.collection('rooms');
@@ -68,16 +94,67 @@ async function run() {
       res.json(result);
     })
 
-    app.post('/booking', async (req, res) => {
-      const bookingData = req.body;
-      const result = await bookingCollection.insertOne(bookingData);
+    app.get('/booking/:userId', async (req, res) => {
+      const {userId} = req.params;
+
+      const result = await bookingCollection.find({userId: userId}).toArray();
+      res.json(result);
+    })    
+
+    // conflict check start
+    
+    app.post("/booking", verifyToken, async (req, res) => {
+  const booking = req.body;
+
+  const existingBookings = await bookingCollection.find({
+    roomId: booking.roomId,
+    date: booking.date,
+    status: "confirmed",
+  }).toArray();
+
+  const isBooked = existingBookings.some(existing => {
+
+        const existingStart = Number(existing.startTime.split(":")[0]);
+        const existingEnd = Number(existing.endTime.split(":")[0]);
+
+        const newStart = Number(booking.startTime.split(":")[0]);
+        const newEnd = Number(booking.endTime.split(":")[0]);
+
+        return (
+          newStart < existingEnd &&
+          newEnd > existingStart
+        );
+        // console.log("Found bookings:", existingBookings);
+      });
+      
+
+      if (isBooked) {
+        return res.status(409).json({
+          success: false,
+          message: "This room is already booked for the selected time."
+        });
+      }
+
+  const result = await bookingCollection.insertOne(booking);
+  
+
+  res.status(201).json({
+    message: "Room booked successfully",
+    result,
+  });
+});
+// conflict check end
+
+
+    app.delete('/booking/:bookingId', async (req, res) => {
+      const { bookingId } = req.params;
+      const result = await bookingCollection.deleteOne({_id: new ObjectId(bookingId)})
 
       res.json(result);
-    })
-
+    })    
 
     
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
